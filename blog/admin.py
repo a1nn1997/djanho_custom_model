35ce0c0e1590b0b317c3e067581fefe8822eafd3
Aniruddh Singh <asb.aniruddh.singh@gmail.com>
2022-04-25 00:35:01 +0530

    unregister models commit 4

--- blog/admin.py
+++ blog/admin.py
@@ -33,10 +33,12 @@ from .models import Post,Category
 
 import django.apps
 
+
 models = django.apps.apps.get_models()
-#print(models)
+print(models)
 for model in models:
     try:
         admin.site.register(model)
     except admin.sites.AlreadyRegistered:
         pass
+admin.site.unregister(django.contrib.sessions.models.Session)
