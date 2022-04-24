35ed08022f77bf526088753e5333c2f8c42c5c3a
Aniruddh Singh <asb.aniruddh.singh@gmail.com>
2022-04-25 00:43:49 +0530

    custom field part1 basics commit 1

--- blog/admin.py
+++ blog/admin.py
@@ -1,44 +1,13 @@
 from dataclasses import field
 from django.contrib import admin
 from .models import Post,Category
-# Register your models here.
 
-#  part 1
-# class BlogAdminArea(admin.AdminSite):
-#     site_header = 'Blog Admin Area'
+# ( ) will bring the part in one line
 
+class PostAdmin(admin.ModelAdmin):
+    #fields=['title','author']
+    fields=['title',('author','slug')]
 
-# blog_site = BlogAdminArea(name='BlogAdmin')
+admin.site.register(Post, PostAdmin)
 
-# admin.site.register(models.Post)
-
-# blog_site.register(models.Post)
-
-# admin.site.register(models.Category)
-
-# blog_site.register(models.Category)
-
-# part 2
-
-    # way 1
-# class PostAdmin(admin.ModelAdmin):
-#     fields=['title','author']
-
-# admin.site.register(Post, PostAdmin)
-
-    #way 2
-# @admin.register(Post)
-# class PostAdmin(admin.ModelAdmin):
-#     fields=['title','author']
-
-import django.apps
-
-
-models = django.apps.apps.get_models()
-print(models)
-for model in models:
-    try:
-        admin.site.register(model)
-    except admin.sites.AlreadyRegistered:
-        pass
-admin.site.unregister(django.contrib.sessions.models.Session)
+admin.site.register(Category)
