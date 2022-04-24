e763f2f817df961babacfef8d9941d5a3c75d72f
Aniruddh Singh <asb.aniruddh.singh@gmail.com>
2022-04-25 00:52:41 +0530

    adding sections and their customization using fieldsets commit 2

--- blog/admin.py
+++ blog/admin.py
@@ -3,10 +3,20 @@ from django.contrib import admin
 from .models import Post,Category
 
 # ( ) will bring the part in one line
-
+TEXT = 'THIS IS TEXT'
 class PostAdmin(admin.ModelAdmin):
     #fields=['title','author']
-    fields=['title',('author','slug')]
+    #fields=['title',('author','slug')]
+    fieldsets =(
+        ('Section 1',{
+            'fields': ('title','author',),
+            'description': '%s' % TEXT,
+        }),
+        ('Section 2',{
+            'fields': ('slug',),
+            'classes':('collapse','wide',),
+        })
+    )
 
 admin.site.register(Post, PostAdmin)
 
